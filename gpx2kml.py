4235725bbe951ffc8d76df924322d07142b75e69
jeremie <20644775+jdespraz@users.noreply.github.com>
2022-09-04 22:23:40 +0200

    version 2 -- using simplekml library and adding animations

--- gpx2kml.py
+++ gpx2kml.py
@@ -6,19 +6,86 @@ from lxml import etree
 from pykml.factory import KML_ElementMaker as KML
 from pykml.factory import ATOM_ElementMaker as ATOM
 from pykml.factory import GX_ElementMaker as GX
+import simplekml as skml
+import numpy as np
+from math import pi,sqrt,exp,atan2
 
 # Script parameters
 # -------------------------
 verbose = False
 inputFile = 'input.gpx'
-outputFile = 'output.kml'
+outputFile = 'tour.kml'
+model3DFile = 'old_plane.dae'
+modelScale = 10
+viewTilt = 120
+smoothFactor = 4 # (1 is no smoothing)
 altitudeMode = 'absolute'
-altitudeOffset = 0
-extrude = 1
-tesselate = 1
+extrude = 0
+tessellate = 1
+rad2deg = 180.0/pi
+deg2rad = 1/rad2deg
+location = 'Charmey'
 
-# Parsing an existing file:
-# -------------------------
+locations = {'Charmey': {'start': [7.2058246, 46.6251042, 1606.192797], 
+                         'stop': [7.1698914, 46.6181322, 872.5266369]}}
+
+# 
+# Functions definitions
+# 
+
+def crop_flight(pts, start=None, stop=None, loc=None):
+    if start is None or stop is None:
+        x = np.array([p.longitude for p in pts])
+        y = np.array([p.latitude for p in pts])
+        ri = (x-loc['start'][0])**2 + (y-loc['start'][1])**2
+        rf = (x-loc['stop'][0])**2 + (y-loc['stop'][1])**2
+        start = np.argmin(ri)
+        stop = np.argmin(rf)
+        print('auto start idx:', start)
+        print('auto stop idx:', stop)
+    new_pts = []
+    for k in range(start, stop):
+        new_pts.append(pts[k])
+    return new_pts
+
+def get_heading(pts, k):
+    if k+1 >= len(pts):
+        print(f'Warning in get_heading(): index {k} out of bounds')
+        return 0
+    dlat = (pts[k+1].latitude - pts[k].latitude) * deg2rad
+    dlon = (pts[k+1].longitude - pts[k].longitude) * deg2rad
+    heading = atan2(dlon, dlat) * rad2deg # 0 deg is North
+    return np.mod(heading, 360)
+
+def get_roll(pts, k):
+    dheading = pts[k+1] - pts[k]
+    dheading_max = 45
+    roll = dheading / d_heading_max * 90
+    return roll
+
+def correct_altitude(pts, hi, hf):
+    offset_i = pts[0].elevation - hi
+    offset_f = pts[-1].elevation - hf
+    offset = np.linspace(offset_i, offset_f, len(pts))
+    for k in range(len(pts)):
+        pts[k].elevation -= offset[k]
+        pts[k].elevation = np.maximum(pts[k].elevation, 0)
+    return pts
+
+def smooth_points(pts, length=2):
+    pts_sm = []
+    for k in range(0, len(pts)):
+        stop = np.minimum(k+length, len(pts))
+        lat = np.median([p.latitude for p in pts[k:stop]])
+        lon = np.median([p.longitude for p in pts[k:stop]])
+        h = np.median([p.elevation for p in pts[k:stop]])
+        time = pts[k].time
+        pts_sm.append(gpxpy.gpx.GPXTrackPoint(lat, lon, h, time))
+    return pts_sm
+
+# 
+# Parsing gpx file
+# 
 
 gpx_file = open(os.path.join('./gpx',inputFile), 'r')
 gpx = gpxpy.parse(gpx_file)
@@ -48,33 +115,87 @@ for route in gpx.routes:
 
 kml_coords_str = ""
 for point in points:
-    kml_coords_str += f'{point.longitude},{point.latitude},{point.elevation + altitudeOffset}{os.linesep}'
+    kml_coords_str += f'{point.longitude},{point.latitude},{point.elevation}{os.linesep}'
 
-doc = KML.kml(
-  etree.Comment(' required when using gx-prefixed elements '),
-  KML.Placemark(
-    KML.name('gx:altitudeMode Example'),
-    KML.LookAt(
-      KML.longitude('146.806'),
-      KML.latitude('12.219'),
-      KML.heading('-60'),
-      KML.tilt('70'),
-      KML.range('6300'),
-      GX.altitudeMode(altitudeMode),
-    ),
-    KML.LineString(
-      KML.extrude(extrude),
-      GX.altitudeMode(altitudeMode),
-      KML.coordinates(
-        kml_coords_str
-      ),
-    ),
-  ),
-)
-doc_str = etree.tostring(etree.ElementTree(doc), pretty_print=True).decode('UTF-8')
+points = crop_flight(points, 420, 900, locations[location])
+points = smooth_points(points, smoothFactor)
+points = correct_altitude(points, locations[location]['start'][2], 
+                                  locations[location]['stop'][2])
 
-with open(os.path.join('./kml',outputFile), 'w') as kml_file:
-    if verbose:
-        print(doc_str)
-    kml_file.write(doc_str)
+#
+# generating kml
+#
+kml = skml.Kml(name='Trajectories',open=1)
+
+tour = kml.newgxtour(name=gpx.tracks[0].name)
+playlist = tour.newgxplaylist()
+
+model = kml.newmodel(altitudemode=altitudeMode)
+#model.link.href = os.path.join('./models',model3DFile)
+model.link.href = model3DFile
+model.name = 'Parapente'
+model.scale.x = modelScale
+model.scale.y = modelScale
+model.scale.z = modelScale
+
+flyto = playlist.newgxflyto(gxduration=0.0)
+flyto.lookat.altitudemode = altitudeMode
+flyto.lookat.longitude = points[0].longitude
+flyto.lookat.latitude = points[0].latitude
+flyto.lookat.altitude = points[0].elevation
+flyto.lookat.heading = get_heading(points, 0)
+flyto.lookat.range = modelScale*4.0
+flyto.lookat.tilt = 0
+flyto.lookat.roll = 0
+
+print('computing Parapente kml data')
+for k in range(len(points)-1):
+    heading = get_heading(points, k)
+    animatedupdate = playlist.newgxanimatedupdate(gxduration=0.0)
+    animatedupdate.update.change = '<Orientation targetId="parapente_orientation">' + \
+                                            '<heading>' + str(model.orientation.heading) + '</heading>' + \
+                                            '</Orientation>'
+    model.location.longitude = points[k].longitude
+    model.location.latitude = points[k].latitude
+    model.location.altitude = points[k].elevation
+    model.orientation.heading = heading
+    model.orientation.tilt = 0 #-x['aoa'][k]*rad2deg-x['gam'][k]*rad2deg
+    model.orientation.roll = 0 #-x['bank'][k]*rad2deg*np.cos(-x['gam'][k])
+    animatedupdate = playlist.newgxanimatedupdate(gxduration=1.0)
+    #scale = x['v'][k]*(100.0/90.0)
+    animatedupdate.update.change = '<Location targetId="parapente_location"> <altitude>' + \
+                                       str(model.location.altitude) + '</altitude>' + \
+                                       '<longitude>' + str(model.location.longitude) + '</longitude>' + \
+                                       '<latitude>' + str(model.location.latitude) + '</latitude>' + \
+                       '</Location>' + '<Orientation targetId="parapente_orientation">' + \
+                       '<heading>' + str(model.orientation.heading) + '</heading>' + \
+                       '<tilt>' + str(model.orientation.tilt) + '</tilt>' + \
+                       '<roll>' + str(model.orientation.roll) + '</roll>' + '</Orientation>' + \
+                       '<Scale targetId="parapente_scale">' + '<x>' + str(modelScale) + '</x>' + \
+                       '<y>' + str(modelScale) + '</y>' + '<z>' + str(modelScale) + '</z>' + '</Scale>'
+    flyto = playlist.newgxflyto(gxduration=1.0, gxflytomode='smooth')
+    flyto.lookat.altitudemode = altitudeMode
+    flyto.lookat.longitude = points[k].longitude
+    flyto.lookat.latitude = points[k].latitude
+    flyto.lookat.altitude = points[k].elevation
+    flyto.lookat.heading =  heading #-x['bank'][k]*np.sin(-x['gam'][k])*rad2deg
+    flyto.lookat.range = modelScale*4.0
+    flyto.lookat.tilt = viewTilt #+(x['aoa'][k]*rad2deg+x['gam'][k]*rad2deg)/2.0
+    flyto.lookat.roll = 0
+
+#
+# Create a linstring with the full trajectory
+#
+
+linestring = kml.newlinestring(name="Trajectoire")
+linestring.altitudemode = altitudeMode
+linestring.extrude = extrude
+linestring.tessellate = tessellate
+linestring.coords = [(p.longitude, p.latitude, p.elevation) for p in points]
+
+#
+# save
+#
+print('writing kml file')
+kml.save(os.path.join('./kml',outputFile))
 
